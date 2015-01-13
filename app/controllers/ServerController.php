913b564ccdd784492ad2e27f80aeac5efa11fb80
martinappo <martinappo@gmail.com>
2015-01-13 18:32:22 +0200

    test

--- app/controllers/ServerController.php
+++ app/controllers/ServerController.php
@@ -5,7 +5,8 @@ class ServerController extends BaseController {
 	public function deploy() {
 		SSH::into('production')->run(array(
 			'cd ~/domains/crisp.ee/crispLaravel',
-			'git pull origin master'
+			'git pull origin master',
+			'"yes"| cp -r public_html/* ../public_html'
 		), function ($line) {
 			echo $line.PHP_EOL;
 		});
