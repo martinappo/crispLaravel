b63c6a815f3b4b4a091387e91076960793051c60
martinappo <martinappo@gmail.com>
2015-01-19 15:09:44 +0200

    large navbar

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -14,7 +14,7 @@
 		<div class="container">
 			<nav class="navbar navbar-default" role="navigation">
 				<div class="container-fluid">
-					<div class="navbar-header col-md-10 col-sm-10">
+					<div class="navbar-header col-md-10 col-sm-10 col-lg-12">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 							<span class="sr-only">Toggle navigation</span>
 							<span class="icon-bar"></span>
@@ -24,13 +24,11 @@
 						<a class="navbar-brand col-sm-10 col-md-10 col-xs-10" href="[[ URL::to('/') ]]"></a>
 					</div>
 					<div id="navbar" class="navbar-collapse collapse">
-
 						<ul class="nav navbar-nav navbar-right">
 							<li class="active"><a href="#">Meist</a></li>
 							<li><a href="#">Portfolio</a></li>
 							<li><a href="#">Kontakt</a></li>
 						</ul>
-
 					</div><!--/.nav-collapse -->
 				</div><!--/.container-fluid -->
 			</nav>
