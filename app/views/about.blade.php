59e462135655f1c8f9750166930be3c599002342
martinappo <martinappo@gmail.com>
2015-03-23 16:14:39 +0200

    About page corrections.

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -4,48 +4,48 @@
 
 	<div class="container-fluid">
 		<section class="content col-lg-14 col-centered">
-			<div class="col-md-10">
-				<h1>
-					Veebirakendused<span class="big">/</span> <br>
-					graafiline disain
-				</h1>
-				<article class="text">
-					<p>
-						Crisp on loovkollektiiv, mille tõukeallikaks on 
-						<span style="letter-spacing: 1px;">
-							hea ja arusaadava kuvandi ning tugeva
+			<div class="inner">
+				<div class="col-md-10 about-description">
+					<h1>
+						<span class="bold">
+							Veebiloome &amp; <br>
+							graafiline disain
 						</span>
-						kommunikatiivse keskkonna loomine. 
-					</p>
-					<p>
-						Oleme kõige lihtsamal moel pühendunud andma nii sisulise kui välise elu mistahes pinnale, tootele või teenusele, et nii visuaalse kui sisulise väärtuse lugemine oleks lihtne.
-					</p>
-				</article>
-			</div>
+					</h1>
+					<article class="text">
+						<p>
+							Crisp on loovkollektiiv, mille tõukeallikaks on 
+							hea ja arusaadava kuvandi ning tugeva
+							kommunikatiivse keskkonna loomine. 
+						</p>
 
-			<div class="col-md-10 about persons">
-				<div class="col-md-10 person">
-					<a href="mailto:martin.appo@crisp.ee">
-						<div class="person-foto"></div>
-					</a>
-					<div class="person-name person-info">Martin Appo</div>
-					<div class="preson-desc person-info">Veebiarendus</div>
+					</article>
 				</div>
 
-				<div class="col-md-10 person">
-					<a href="mailto:heigo.heinleht@crisp.ee">
-						<div class="person-foto"></div>
-					</a>
-					<div class="person-name person-info">Heigo Heinleht</div>
-					<div class="preson-desc person-info">Graafiline disain</div>
-				</div>
+				<div class="col-md-10 about persons">
+					<div class="col-md-10 person">
+						<a href="mailto:martin.appo@crisp.ee">
+							<div class="person-foto" style="background-image: url(../img/m_icon.png);"></div>
+						</a>
+						<div class="person-name person-info">Martin Appo</div>
+						<div class="preson-desc person-info">Veebiarendus</div>
+					</div>
 
-				<div class="col-md-10 person">
-					<a href="mailto:tanel.tomson@crisp.ee">
-						<div class="person-foto"></div>
-					</a>
-					<div class="person-name person-info">Tanel Tomson</div>
-					<div class="preson-desc person-info">Veebiarendus</div>
+					<div class="col-md-10 person">
+						<a href="mailto:heigo.heinleht@crisp.ee">
+							<div class="person-foto" style="background-image: url(../img/h_icon.png);"></div>
+						</a>
+						<div class="person-name person-info">Heigo Heinleht</div>
+						<div class="preson-desc person-info">Graafiline disain</div>
+					</div>
+
+					<div class="col-md-10 person">
+						<a href="mailto:tanel.tomson@crisp.ee">
+							<div class="person-foto" style="background-image: url(../img/t_icon.png);"></div>
+						</a>
+						<div class="person-name person-info">Tanel Tomson</div>
+						<div class="preson-desc person-info">Veebiarendus</div>
+					</div>
 				</div>
 			</div>
 		</section>
